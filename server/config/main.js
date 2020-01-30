02fb0596a0edd9128de26dde976e9d851d4966ad
SamuelDuke <samuelwinterton@gmail.com>
2020-01-30 15:22:59 -0700

    Delete main.js

--- server/config/main.js
+++
@@ -1,9 +0,0 @@
-const databaseName = "basic_mern_app";
-
-module.exports = {
-  port: 8080, //process.env.PORT || 8080,
-  database: `mongodb://localhost:27017/${databaseName}`,
-  jwtSecret:
-    "I am Samuel Duke Winterton I choose the right and I love Erica Ann Winterton",
-  jwtExpiration: 1000000
-};
